3e98e6b4994a69f343a10b6fee83a36740888059
Rishu <rishabhkaparwan8@gmail.com>
2024-11-11 14:12:54 +0530

    11th NOV,2024

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -23,6 +23,9 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        '''
+        This Function is responsible for Data Transformation
+        '''
         try:
             numerical_columns=['reading_score', 'writing_score']
             categorical_columns=[
@@ -33,6 +36,67 @@ class DataTransformation:
                 'test_preparation_course'
             ]
 
+            num_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="median")),# to deal with outliers
+                    ("scaler",StandardScaler())
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler())
+                ]
+            )
+
+            logging.info(f"Numerical Columns :{numerical_columns}")
+
+            logging.info(f"Categorical Columns :{categorical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipelines",cat_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def intitiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read Train and Test Data Completed")
+            logging.info("Obtaining Preprocessing Object")
+
+            preprocesing_obj=self.get_data_transformer_object()
+
+            target_column_name="math_score"
+            numerical_columns=["writing_score","reading_score"]
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name])
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name])
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(f"Applying Preprocessing object on training Dataframe and Training Dataframe.")
+
+            input_feature_train_arr=preprocesing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocesing_obj.transform(input_feature_test_df)
+
+            # This code snippet horizontally concatenates (np.c_[]) two arrays: input_feature_train_arr and target_feature_train_df, combining them into a single array, train_arr.
+            train_arr=np.c_[
+                input_feature_train_arr,np.array(target_feature_train_df)
+            ]
+
             
+
         except:
             pass
