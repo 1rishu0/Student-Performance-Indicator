6d14cfb0db0d39c8175704fbc6920f03ac9dfddd
Rishu <rishabhkaparwan8@gmail.com>
2024-11-14 13:44:34 +0530

    14th NOV,2024

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -23,3 +23,28 @@ class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Split Training and Test Input Data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models={
+                "Random Forest":RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regression":LinearRegression(),
+                "K-Neighbors Classifier":KNeighborsRegressor(),
+                "XGBClassifier":XGBRegressor(),
+                "CatBoosting Classifier":CatBoostRegressor(),
+                "AdaBoost Classifier":AdaBoostRegressor()
+            }
+        except:
+            pass
