c34b246fded294847e83292e0fcbfc2104f4b8eb
Rishu <rishabhkaparwan8@gmail.com>
2024-11-20 05:08:13 +0530

    20th NOV,2024

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,37 +43,38 @@ class ModelTrainer:
                 "Decision Tree":DecisionTreeRegressor(),
                 "Gradient Boosting":GradientBoostingRegressor(),
                 "Linear Regression":LinearRegression(),
-                "K-Neighbors Classifier":KNeighborsRegressor(),
-                "XGBClassifier":XGBRegressor(),
-                "CatBoosting Classifier":CatBoostRegressor(),
-                "AdaBoost Classifier":AdaBoostRegressor()
+                "K-Neighbors Regressor":KNeighborsRegressor(),
+                "XGB Regressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(),
+                "AdaBoost Regressor":AdaBoostRegressor()
             }
 
             params={
-                "Decision Tree":{
-                    "criterion":["squared_error","friedman_mse","absolute_error","poisson"],
-                    # "splitter":["best","random"],
-                    "max_features":["sqrt","log2"]
-                },
                 "Random Forest":{
                     # "criterion":["squared_error","friedman_mse","absolute_error","poisson"],
                     # "max_features":["sqrt","log2","None"],
                     "n_estimators":[8,16,32,64,128,256]
                 },
+                "Decision Tree":{
+                    "criterion":["squared_error","friedman_mse","absolute_error","poisson"],
+                    # "splitter":["best","random"],
+                    "max_features":["sqrt","log2"]
+                },
                 "Gradient Boosting":{
                     # "loss":["squared_error","huber","absolute_error","quantile"],
                     "learning_rate":[.1,.01,.05,.001],
-                    "subsample":[0.6,0.7,0.75,.0.8,0.85,0.9],
+                    "subsample":[0.6,0.7,0.75,.08,0.85,0.9],
                     # "criterion":["squared_error","friedman_mse"],
                     # "max_features":["auto","sqrt","log2"],
                     "n_estimators":[8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
-                "XGBRegressor":{
+                "K-Neighbors Regressor":{},
+                "XGB Regressor":{
                     "learning_rate":[.1,.01,.05,.001],
                     "n_estimators":[8,16,32,64,128,256]
                 },
-                "CatBoosting Regressior":{
+                "CatBoosting Regressor":{
                     "depth":[6,8,10],
                     "learning_rate":[0.01,0.05,0.1],
                     "iterations":[30,50,100]
